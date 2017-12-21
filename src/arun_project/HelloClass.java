84ee966c0663270700a681db6ee9f44e25426aa4
Arun Das <chitaarun@gmail.com>
2017-12-20 23:53:29 -0500

    hello class has two program

---
+++ src/arun_project/HelloClass.java
@@ -0,0 +1,12 @@
+package arun_project;
+
+public class HelloClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Students are in Selenium class");
+
+System.out.println("They want to learn java program");
+	}
+
+}
