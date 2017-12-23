41a5d3dda934269a9e6638baecf0b90743d8a89c
Arun Das <chitaarun@gmail.com>
2017-12-23 12:40:33 -0500

    files add

---
+++ src/arun_project/Car.java
@@ -0,0 +1,17 @@
+package arun_project;
+
+public class Car {
+	
+	
+	private void Car() {
+		System.out.println("Car has wheel");
+		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
