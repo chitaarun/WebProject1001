c8a88b808f97665cb37d06a54094b1000df89d2d
chitaarun <chitaarun@gmail.com>
2018-01-02 00:23:31 -0500

    first friend added

---
+++ src/FirstfriendJava.java
@@ -0,0 +1,10 @@
+public class FirstfriendJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Create a git hub account....");
+		System.out.println("Generate a ssh key is not too much easy");
+		System.out.println("Add ssh key in your git hub account");
+	}
