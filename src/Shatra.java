7966ee1d3777ddd0f05610fdf2e99814b6c65a53
chitaarun <chitaarun@gmail.com>
2018-01-17 21:46:39 -0500

    untracked file

---
+++ src/Shatra.java
@@ -0,0 +1,28 @@
+
+public class Shatra {
+   
+	
+	int  Roll;
+	
+	String Name;
+	Float Mark;
+	Shatra (int R,String N, Float M){
+		Roll=102;
+		Name = "Raja";
+		Mark =85.5f;
+		
+		System.out.println("Roll is : " +Roll);
+		System.out.println("Name is :" +Name);
+		System.out.println("Mark is:"  +Mark);
+
+	
+	System.out.println("Roll is : " +Roll);
+	System.out.println("Name is :" +Name);
+	System.out.println("Mark is:"  +Mark);
+
+	}
+public static void main(String[]args){
+
+	Shatra s = new Shatra(102,"raja",85.5f);
+}
+}
